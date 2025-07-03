5aa3a4cca5e953b5f376762e4d46a35b31cb022d
DartVlayder <mr.6990@mail.ru>
2025-07-03 20:16:29 +0300

    Добавил переход из каталога на конкретный товар

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -5,6 +5,6 @@ from goods import views
 app_name = 'goods'
 urlpatterns = [
     path('', views.catalog, name='index'),
-    path('product', views.product, name='product'),
+    path('product/<int:product_id>/', views.product, name='product'),
 
 ]
