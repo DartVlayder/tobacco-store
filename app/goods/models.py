9612f9e69672359bb47fa1def0fc569108ee584c
DartVlayder <mr.6990@mail.ru>
2025-07-17 19:50:04 +0300

    Закончил работу с полнотекстовым поиском

--- app/goods/models.py
+++ app/goods/models.py
@@ -15,6 +15,7 @@ class Categories(models.Model):
 
 
 class Products(models.Model):
+    objects = None
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
