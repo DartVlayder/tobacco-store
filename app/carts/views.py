65247b4990756be7b2e9b419a341b3e878ff111b
DartVlayder <mr.6990@mail.ru>
2025-07-27 18:07:25 +0300

    Добавил просмотр корзины

--- app/carts/views.py
+++ app/carts/views.py
@@ -1,7 +1,24 @@
+from django.shortcuts import render, redirect
+from sqlparse.utils import offset
+
 from django.shortcuts import render
+from carts.models import Cart
+from goods.models import Products
+
 
 def cart_add(request, product_slug):
-    ...
+    product = Products.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+        if carts.exists():
+            carts = carts.first()
+            if carts:
+                carts.quantity += 1
+                carts.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+    return redirect(request.META['HTTP_REFERER'])
 
 def cart_change(request, product_slug):
     ...
