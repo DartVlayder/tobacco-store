ab457b690d7be0b362f727f25ab69903352402f9
DartVlayder <mr.6990@mail.ru>
2025-07-28 18:14:33 +0300

    Добавлен контроллер удаления корзины

--- app/users/views.py
+++ app/users/views.py
@@ -7,7 +7,6 @@ from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
-
 def login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
@@ -19,7 +18,8 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f'{username}, Вы вошли в аккаунт ')
 
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
 
                 return HttpResponseRedirect(reverse('main:index'))
