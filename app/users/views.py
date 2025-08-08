06194c10e7e5eda24d4ceadb3d631e206d762dab
DartVlayder <mr.6990@mail.ru>
2025-08-08 20:39:20 +0300

    Правки

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
-
-from carts.apps import CartsConfig
 from carts.models import Cart
+from orders.models import Order, OrderItem
+
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
@@ -54,7 +55,6 @@ def registration(request):
 
             if session_key:
                 Cart.objects.filter(session_key=session_key).update(user=user)
-
             messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
@@ -65,6 +65,7 @@ def registration(request):
         'form': form
     }
     return render(request, 'users/registration.html', context)
+
 
 @login_required
 def profile(request):
@@ -77,11 +78,20 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
 
+    orders = Order.objects.filter(user=request.user).prefetch_related(
+        Prefetch(
+            "orderitem_set",
+            queryset=OrderItem.objects.select_related("product"),
+        )
+    ).order_by("-id")
+
     context = {
         'title': 'Home - Кабинет',
-        'form': form
+        'form': form,
+        'orders': orders,
     }
     return render(request, 'users/profile.html', context)
+
 
 def users_cart(request):
     return render(request, 'users/users_cart.html')
