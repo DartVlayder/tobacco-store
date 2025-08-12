a31a2965d354f97081c91e9be67197cedbedf328
DartVlayder <mr.6990@mail.ru>
2025-08-12 21:40:18 +0300

    Работа с классами в приложении users

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,19 +1,43 @@
 from django.http import Http404
 from django.views.generic import DetailView, ListView
 
-from goods.models import Products
+from goods.models import Products, Categories
 from goods.utils import q_search
 
 class CatalogView(ListView):
     model = Products
-    template_name = 'catalog/index.html'
+    template_name = 'goods/catalog.html'
     context_object_name = 'goods'
     paginate_by = 3
+    allow_empty = False
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug')
+        on_sale = self.request.GET.get('on_sale')
+        order_by = self.request.GET.get('order_by')
+        query = self.request.GET.get('q')
+
+        if category_slug == 'all':
+            goods = super().get_queryset()
+        elif query:
+            goods=q_search(query)
+        else:
+            goods = super().get_queryset().filter(category__slug=category_slug)
+            if not goods.exists():
+                raise Http404()
+
+        if on_sale:
+            goods = goods.filter(discount__gt=0)
+
+        if order_by and order_by != 'default':
+            goods = goods.order_by(order_by)
+
+        return goods
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home - Каталог'
-        context['slug_url'] = 'asdasfdagw'
+        context['slug_url'] = self.kwargs.get('category_slug')
         return context
 
 # def catalog(request, category_slug=None):
