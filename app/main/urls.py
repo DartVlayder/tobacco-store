714281d1088ad22784b45107b3d4baa59f18c338
DartVlayder <mr.6990@mail.ru>
2025-08-14 17:46:00 +0300

    Реализовал кэширование

--- app/main/urls.py
+++ app/main/urls.py
@@ -5,6 +5,5 @@ from main import views
 app_name = 'main'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('about', views.AboutView.as_view() , name='about'),
-
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
